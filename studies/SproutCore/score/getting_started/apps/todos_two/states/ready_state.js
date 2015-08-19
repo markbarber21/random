9d731defb4bdb975877ab5cd1b671a2e6cf98776
mbarber <mbarber@companioncorp.com>
2015-08-19 10:21:34 -0600

    SproutCore Basics

---
+++ studies/SproutCore/score/getting_started/apps/todos_two/states/ready_state.js
@@ -0,0 +1,16 @@
+TodosTwo.ReadyState = SC.State.extend({
+  
+  enterState: function() {
+    TodosTwo.getPath('mainPage.mainPane').append();
+  },
+
+  exitState: function() {
+    TodosTwo.getPath('mainPage.mainPane').remove();
+  },
+
+  proveIt: function(someParam) {
+    alert(someParam);
+  }
+
+});
+
